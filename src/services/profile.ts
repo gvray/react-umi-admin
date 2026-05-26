86b5f2fd4a13a8e7c3dc30b831c5c29201ad7913
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2026-05-26 23:18:10 +0800

    fix: queryProfile 参数类型

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,7 +1,7 @@
-import { HttpRequestOptions, request } from '@gvray/request';
+import { RuntimeControlFields, request } from '@gvray/request';
 
 /** 获取当前用户信息 */
-export function queryProfile(options?: HttpRequestOptions) {
+export function queryProfile(options?: RuntimeControlFields) {
   return request<API.Response<API.CurrentUserResponseDto>>('/profile', {
     method: 'GET',
     ...options,
